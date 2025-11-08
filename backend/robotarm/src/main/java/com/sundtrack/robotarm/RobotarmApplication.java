d372069cd749dafde66443ac3d1713cc54be7b62
lynetma2 <lynetma2@users.noreply.github.com>
2025-11-08 22:14:13 +0100

    Initial JAVA backend commit

---
+++ backend/robotarm/src/main/java/com/sundtrack/robotarm/RobotarmApplication.java
@@ -0,0 +1,13 @@
+package com.sundtrack.robotarm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RobotarmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RobotarmApplication.class, args);
+	}
+
+}
---
+++ backend/robotarm/src/test/java/com/sundtrack/robotarm/RobotarmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sundtrack.robotarm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RobotarmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
