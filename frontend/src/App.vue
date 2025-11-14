94b61db098923af1839b7be379253a515f696847
lynetma2 <lynetma2@users.noreply.github.com>
2025-11-14 23:02:14 +0100

    Added initial motor configuration layout

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,4 +1,7 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import "./style.css"
+import MotorCard from "@/components/motorConfiguration/motorCard.vue";
+</script>
 
 <template>
   <h1>You did it!</h1>
@@ -6,6 +9,9 @@
     Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
     documentation
   </p>
+  <div class="flex flex-row min-h-screen justify-center items-start">
+    <MotorCard/>
+  </div>
 </template>
 
 <style scoped></style>
---
+++ frontend/src/components/motorConfiguration/motorCard.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+import {ref} from "vue";
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import {
+  Tabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger,
+} from "@/components/ui/tabs"
+import Tmc2209settings from "@/components/motorConfiguration/tmc2209settings.vue";
+
+const modelValue = ref([50])
+</script>
+
+<template>
+  <Card class="w-[350px]">
+    <CardHeader>
+      <CardTitle>Motor Configuration</CardTitle>
+      <CardDescription>This changes the motor with id = 0</CardDescription>
+    </CardHeader>
+    <CardContent>
+     <tmc2209settings/>
+    </CardContent>
+  </Card>
+</template>
+
+<style scoped>
+
+</style>
